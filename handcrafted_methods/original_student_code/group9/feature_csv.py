c38b807d8227ad608b3788b3b3cd45f30ea3d78b
RejeSeje <andreasmwulff@gmail.com>
2025-03-06 11:37:49 +0100

    Changed absolute paths to relative

--- handcrafted_methods/original_student_code/group9/feature_csv.py
+++ handcrafted_methods/original_student_code/group9/feature_csv.py
@@ -7,10 +7,10 @@ import pandas as pd
 ### COMSTANTS ###
 #################
 
-file_data = '/Users/regitzesydendal/Documents/itu/6 semester/bachelor/data/new_padufes/metadata.csv'
-image_folder = '/Users/regitzesydendal/Documents/itu/6 semester/bachelor/data/new_padufes/imgs_part_1/'
-mask_folder = '/Users/regitzesydendal/Documents/itu/6 semester/bachelor/data/new_padufes/new_padufes_masks/'
-file_features = '/Users/regitzesydendal/Documents/itu/6 semester/bachelor/data/feature_group9_new.csv'
+file_data = 'data/new_padufes/metadata.csv'
+image_folder = 'data/new_padufes/imgs_part_1/'
+mask_folder = 'data/new_padufes/new_padufes_masks/'
+file_features = 'data/feature_group9_new.csv'
 
 feature_names = ['mean_asymmetry', 'best_asymmetry', 'worst_asymmetry', 'red_var', 'green_var', \
      'blue_var', 'hue_var', 'sat_var', 'val_var', 'dom_hue', 'dom_sat', 'dom_val', \
